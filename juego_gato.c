b62dd74c35d42115fee890244318d35d6063b35d
lspci4 <ingluispt@gmail.com>
2025-07-05 14:40:31 -0400

    Agregada funcion llena tablero

--- juego_gato.c
+++ juego_gato.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 
+void inicializa_tablero(char tablero[3][3]);
+
 int main()
 {
     // Declaración del tablero 3x3
-    char matriz[3][3];
+    char tablero[3][3];
+    
+    inicializa_tablero(tablero);
     
 	return 0;
+}
+
+// llena el tablero con los caracteres del '1' al '9'
+void inicializa_tablero(char tablero[3][3]){
+    int fila, columna;
+    char celda = '1';
+    
+    for(fila = 0; fila < 3; fila++){
+        for(columna = 0; columna < 3; columna++){
+            tablero[fila][columna] = celda++;
+            printf("%c", tablero[fila][columna]);
+        }
+    }
 }
