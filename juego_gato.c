dd39e045010ce47206f15de0a5b8e495bbee00c4
lspci4 <ingluispt@gmail.com>
2025-07-05 12:51:55 -0400

    Declaración de tablero 3x3 en main

--- juego_gato.c
+++ juego_gato.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
+void inicializar_tablero(char matriz[3][3]);
+
+int main()
 {
-	printf("hello world\n");
+    // Declaración del tablero 3x3
+    char matriz[3][3];
+    
+    inicializar_tablero(matriz);
+    
 	return 0;
+}
+
+// llena el tablero con los caracteres del '1' al '9'
+void inicializar_tablero(char matriz[3][3]){
+    int i, j;
+    char aux = '1';
+    
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            matriz[i][j] = aux++;
+            printf("%c\n", matriz[i][j]);
+        }
+        
+    }
 }
